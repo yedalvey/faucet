c8a5ae10232b7ec03be8aac29631cc8742f132c8
yecai <--local>
2023-09-04 14:29:11 +0800

    export to out

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
+  output: "export",
   webpack: (config) => {
     config.resolve.fallback = { fs: false, net: false, tls: false }
     return config
